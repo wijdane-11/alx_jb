8576b92d9098fd5f427d932f01941cdf9197eddf
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-01-29 11:18:22 +0100

    up

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+// 1-multi_languages.js
+
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
