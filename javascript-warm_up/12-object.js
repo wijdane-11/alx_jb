66b309389d032bacef42fa38f8f0d5f9426baab7
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-01-29 11:33:19 +0100

    up

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+  
+  // Replace the value from 12 to 89
+  myObject.value = 89;
+  
+  console.log(myObject);
+  
