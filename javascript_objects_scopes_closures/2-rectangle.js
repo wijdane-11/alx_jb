bfb896040ab1598b7e67f5839e3fe894379c5df3
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-01-29 12:52:55 +0100

    up

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,22 +1,14 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      } else {
-        // Create an empty object
-        this.width = undefined;
-        this.height = undefined;
-      }
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.create(null);
     }
   }
-  
-  // Override the inspect method for util.inspect
-  Rectangle.prototype.inspect = function () {
-    return 'Rectangle {}';
-  };
-  
-  module.exports = Rectangle;
-  
+}
+
+module.exports = Rectangle;
