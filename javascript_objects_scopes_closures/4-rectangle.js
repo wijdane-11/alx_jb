6f62d293bf5b2141a8ddea1e029c42fa86c9f6f6
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-01-29 12:38:42 +0100

    up

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,41 +1,7 @@
-// 4-rectangle.js
-
-#!/usr/bin/node
-
-class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      // Create an empty object
-      this.width = undefined;
-      this.height = undefined;
-    }
-  }
-
-  print() {
-    if (this.width !== undefined && this.height !== undefined) {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
-  }
-
-  rotate() {
-    if (this.width !== undefined && this.height !== undefined) {
-      // Exchange width and height
-      [this.width, this.height] = [this.height, this.width];
-    }
-  }
-
-  double() {
-    if (this.width !== undefined && this.height !== undefined) {
-      // Double width and height
-      this.width *= 2;
-      this.height *= 2;
-    }
-  }
-}
-
 module.exports = Rectangle;
+// main_0.js
+
+const Rectangle = require('./4-rectangle');
+
+const r1 = new Rectangle(5, 4);
+r1.print();
