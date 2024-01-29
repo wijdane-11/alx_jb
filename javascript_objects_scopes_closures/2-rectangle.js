b2dd94410b85e52691c1b1d05b6aae276da3c441
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-01-29 12:19:57 +0100

    up

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,17 +1,3 @@
-#!/usr/bin/node
-
-class Rectangle {
-    constructor(w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      } else {
-        // Create an empty object
-        this.width = undefined;
-        this.height = undefined;
-      }
-    }
-  }
-  
-  module.exports = Rectangle;
-  
+Rectangle {}
+undefined
+undefined
