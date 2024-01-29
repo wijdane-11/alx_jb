f1f78a46ebb40c3e9918baaa08a05033b0fe1b69
essafi.wijdane49@gmail.com <essafi.wijdane49@gmail.com>
2024-01-29 11:31:26 +0100

    UP

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+// 6-multi_languages_loop.js
+
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let output = "";
+
+for (const line of lines) {
+  output += line + "\n";
+}
+
+console.log(output.trim());
